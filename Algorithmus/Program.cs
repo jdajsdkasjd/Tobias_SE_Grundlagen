e86f67a2535823a0359271b47a89992cb7c2eb47
amir.abudouleh@web.de <amir.abudouleh@training.lug-ag.de>
2025-04-07 14:22:31 +0200

    implemented cardfinding algo

--- Algorithmus/Program.cs
+++ Algorithmus/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection.Emit;
+
+int[] cards = [3, 5, 7, 8, 9];
+int query = 7;
+
+var result = FindCardLocation(cards, query);
+Console.WriteLine(result);
+
+int FindCardLocation(int[] cards, int query)
+{
+    int index = 0;                  
+    while (index < cards.Length)
+        {
+        if (cards[index] == 7)
+        return index;
+        index++;
+        }
+    return -1;    //falls Wert nicht im Array enthalten
+}
